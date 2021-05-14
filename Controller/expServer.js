0bad985943022b6c1efc6278799c33b0f0f23179
Yahav Zarfati <60155625+yahavz100@users.noreply.github.com>
2021-05-14 18:13:51 +0300

    Updated expServer.js file - controller
    Initial REST commands for the assignment - POST,GET.
    Notice all comments still missing implement.
    Check progression of Model and View, aim to connect between them.

--- Controller/expServer.js
+++ Controller/expServer.js
@@ -1,35 +1,55 @@
-const express = require('express')
-const fileUpload = require('express-fileupload')
-const model = require('../Model/SearchInFile')
+const express = require("express");
+const fileupload = require("express-fileupload");
+const model = require("../model/searchInFile");
 
-const app = express()
-app.use(express.urlencoded({
-    extended: false
-}))
+const app = express();
 
-app.use(fileUpload())
-app.use(express.static('../View'))
+/*let commands = new Map();
+commands.set("/", app.get)
+*/
+//Check if need to do like server.js
 
+
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(fileupload());
+// Determine root as ../view
+app.use(express.static("../view"));
+
+//GET retrieves resources
 app.get("/", (req, res) => {
-    res.sendFile('index.html')
-})
+  res.sendFile("./index.html");
+});
 
-app.post('/search', (req, res) => {
-    res.write('searching for ' + req.body.key + ":\n")
-    var key = req.body.key
-    if (req.files) {
-        var file = req.files.text_file
-        var result = model.searchText(key, file.data.toString())
-        res.write(result)
-    }
-    res.end()
-})
-
-app.listen(8080)
+//POST submits new data to the server.
+// input algo - HYBRID/REG, CSV file, another file
+//output JSON - get JSON file from Model to View
+app.post("/detect", (req, res) => {
+  //Check how to do req.body.key - get selected option
+  res.write("Algorithm type:" + "CSV filename:" + "another filename:")
+  //Check if files are valid
+  if(req.files) {
+    //use given algo on files and find anomalies, also return it to view
+  }
+  res.end()
+});
 
 /*
-app.get()
-app.post()
-app.put()
-app.delete()
+//PUT updates existing data.
+app.put("", (req, res) => {
+  //CHECK IF NEED TO DO
+});
+
+//DELETE removes data.
+app.delete("", (req, res) => {
+  //CHECK IF NEED TO DO
+});
 */
+
+
+
+app.listen(8080);
+
