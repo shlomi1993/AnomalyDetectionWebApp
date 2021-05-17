9c123fc1099282ea285df68dc3624241daab2bbc
Yahav Zarfati <60155625+yahavz100@users.noreply.github.com>
2021-05-17 21:45:09 +0300

    Major update for controller
    Finished POST command.
    Wait for View to add threshold input, then send it to Model.
    Issue integrating with model functions.
    Still need to check about PUT,DELETE if needed.
    *minor change in View line 27 - changed /search to /detect (didn't upload View, change it manually).

--- Controller/expServer.js
+++ Controller/expServer.js
@@ -1,41 +1,44 @@
-const express = require("express");
-const fileupload = require("express-fileupload");
-const model = require("../model/searchInFile");
+const express = require("express")
+const fileupload = require("express-fileupload")
+const model = require("../model/AnomalyDetection")
 
-const app = express();
-
-/*let commands = new Map();
-commands.set("/", app.get)
-*/
-//Check if need to do like server.js
-
+const app = express()
 
 app.use(
   express.urlencoded({
     extended: false,
   })
-);
-app.use(fileupload());
+)
+app.use(fileupload())
 // Determine root as ../view
-app.use(express.static("../view"));
+app.use(express.static("../view"))
 
 //GET retrieves resources
 app.get("/", (req, res) => {
-  res.sendFile("./index.html");
-});
+  res.sendFile("./index.html")
+})
 
 //POST submits new data to the server.
-// input algo - HYBRID/REG, CSV file, another file
+//input algo - HYBRID/REG, CSV file, another file
 //output JSON - get JSON file from Model to View
 app.post("/detect", (req, res) => {
-  //Check how to do req.body.key - get selected option
-  res.write("Algorithm type:" + "CSV filename:" + "another filename:")
-  //Check if files are valid
-  if(req.files) {
-    //use given algo on files and find anomalies, also return it to view
+  var algorithm = req.body.algorithm
+  res.write("Algorithm type: " + algorithm + "\n")
+  //Check if files and algorithm are valid
+  if(req.files && algorithm != "None") {
+    var trainFile = req.files.training_file
+    var testFile = req.files.testing_file
+    let choice = 1
+    //Convert user choice for model function
+    if(algorithm == "Hybrid Algorithm") {
+      res.write("USER CHOSE HYBRID\n")
+      choice = 2
+    }
+    var result = model.detectAnomalies(trainFile, testFile, choice, 0.9)
+    res.write(result.stringify())
   }
   res.end()
-});
+})
 
 /*
 //PUT updates existing data.
@@ -49,7 +52,5 @@ app.delete("", (req, res) => {
 });
 */
 
-
-
-app.listen(8080);
+app.listen(8080)
 
