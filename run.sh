848e6549cd2e0c450273c5a8ab70c52c17466a72
shlomi1993 <shlomiggl@gmail.com>
2021-05-20 21:39:11 +0300

    Fix run script

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
+# To run the server, please make sure the following
+# components are installed on your system:
+# 	node v14.17.0or higher version (v16.0.0 recommended)
+# 	npm v7.10.0 or higher version
+# 	node-gyp v8.0.0 or higher version
+
 cd ./Model
 npm install
+node-gyp configure
+node-gyp build
 cd ../Controller
 npm install
-./node expServer.js
+node expServer.js
+echo "Anomaly Detection Server is up!"
